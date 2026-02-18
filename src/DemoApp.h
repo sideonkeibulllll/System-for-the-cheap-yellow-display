adef254a8e2f8872ffa4bb8231da84f743393d32
sideonkeibulllll <kalamaqaq@qq.com>
2026-02-18 23:25:36 +0800

    v1.2.0 正式版本 - DemoApp优化

--- src/DemoApp.h
+++ src/DemoApp.h
@@ -3,27 +3,46 @@
 
 #include "AppManager.h"
 #include "BSP.h"
-#include "PowerManager.h"
 
 class DemoApp : public BaseApp {
 private:
-    lv_obj_t* labelCounter;
-    lv_obj_t* btnInc;
-    lv_obj_t* btnDec;
-    lv_obj_t* btnBack;
-    lv_obj_t* labelPerf;
+    lv_obj_t* tabview;
+    lv_obj_t* keyboard;
+    lv_obj_t* activeTextarea;
     
-    int _counter;
-    uint32_t _lastUpdate;
+    uint32_t _lastUpdateMs;
+    int _animationPhase;
+    bool _keyboardVisible;
     
-    static void btn_inc_cb(lv_event_t* e);
-    static void btn_dec_cb(lv_event_t* e);
+    lv_obj_t* barProgress;
+    lv_obj_t* arcGauge;
+    lv_obj_t* chartWidget;
+    lv_obj_t* labelClock;
+    
+    lv_chart_series_t* chartSeries1;
+    lv_chart_series_t* chartSeries2;
+    
+    static void btn_click_cb(lv_event_t* e);
+    static void slider_cb(lv_event_t* e);
+    static void switch_cb(lv_event_t* e);
+    static void dropdown_cb(lv_event_t* e);
+    static void textarea_cb(lv_event_t* e);
+    static void kb_event_cb(lv_event_t* e);
+    static void colorwheel_cb(lv_event_t* e);
+    static void list_btn_cb(lv_event_t* e);
     static void back_btn_cb(lv_event_t* e);
+    
+    void createBasicTab(lv_obj_t* parent);
+    void createInputTab(lv_obj_t* parent);
+    void createGraphicsTab(lv_obj_t* parent);
+    void createListTab(lv_obj_t* parent);
+    void createColorTab(lv_obj_t* parent);
+    
+    void showKeyboard(lv_obj_t* textarea);
+    void hideKeyboard();
     
 protected:
     virtual bool createUI() override;
-    virtual void saveState() override;
-    virtual bool loadState() override;
     
 public:
     DemoApp();
